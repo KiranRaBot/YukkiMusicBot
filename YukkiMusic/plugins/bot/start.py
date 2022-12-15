00f51fcda61b1ccee63e9849a0cb0c16c64375e1
KiranRaBot <120562614+KiranRaBot@users.noreply.github.com>
2022-12-15 07:45:35 +0400

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -36,7 +36,7 @@ loop = asyncio.get_running_loop()
 
 
 @app.on_message(
-    filters.command(get_command("/start"))
+    filters.command(get_command("INFO_COMMAND"))
     & filters.private
     & ~filters.edited
     & ~BANNED_USERS
@@ -222,7 +222,7 @@ async def start_comm(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(get_command("/start"))
+    filters.command(get_command("INFO_COMMAND"))
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
